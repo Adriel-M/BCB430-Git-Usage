f84c3c4c768eb10174c097edc25ce052c4bc4cad
hyginn <boris.steipe@utoronto.ca>
2017-11-07 15:55:23 -0500

    Added mySeed parameter for set.seed()

--- randSeq.R
+++ randSeq.R
@@ -1,6 +1,7 @@
 # randSeq.R
 
-randSeq <- function(N) {
+randSeq <- function(N, mySeed = 112358) {
   AA <- unlist(strsplit("ACDEFGHKLMNPQRSTVWY", ""))
+  set.seed(mySeed)
   return(sample(AA, N, replace = TRUE))
 }
